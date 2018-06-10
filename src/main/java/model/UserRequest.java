e62990a681e43e195fd443b6bf7d0eef7e7fc4bc
Frame_17 <fedusov.serhiy@gmail.com>
2018-06-10 23:28:15 +0300

    Setting up the starting page

---
+++ src/main/java/model/UserRequest.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserRequest {
+    public static void main(String[] args) {
+        System.out.println(new UserRequest("testRequest"));
+    }
+
+    @Id
+    private String date;
+    private String text;
+
+    public UserRequest() {
+    }
+
+    public UserRequest(String text) {
+        Date now = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        this.date = dateFormat.format(now);
+        this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "date='" + date + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
