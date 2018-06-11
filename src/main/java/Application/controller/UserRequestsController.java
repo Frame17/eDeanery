09d615a4a670270eabadcff5f1cbd06e33bd4485
Frame_17 <fedusov.serhiy@gmail.com>
2018-06-11 18:18:54 +0300

    Slight changes to project structure, mapped requests

---
+++ src/main/java/Application/controller/UserRequestsController.java
@@ -0,0 +1,14 @@
+package Application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UserRequestsController {
+
+    @GetMapping("/hello")
+    public String getMainPage() {
+        return "side";
+    }
+}
