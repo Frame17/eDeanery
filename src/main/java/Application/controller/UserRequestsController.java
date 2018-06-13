8d2909a2cb59c097a617310611dc889c7141ba0c
Frame_17 <fedusov.serhiy@gmail.com>
2018-06-13 23:35:41 +0300

    Done saving and retrieving from the db

--- src/main/java/Application/controller/UserRequestsController.java
+++ src/main/java/Application/controller/UserRequestsController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
@@ -18,6 +20,15 @@ public class UserRequestsController {
 
     @GetMapping("/hello")
     public String getMainPage(Model model) {
+        List<UserRequest> userRequests = userRequestService.findAll();
+        model.addAttribute("userRequests", userRequests);
+        return "pageForStudents";
+    }
+
+    @PostMapping("/hello")
+    public String getFromMainPage(HttpServletRequest request, Model model) {
+        UserRequest userRequest = new UserRequest(request.getParameter("description"));
+        userRequestService.save(userRequest);
         List<UserRequest> userRequests = userRequestService.findAll();
         model.addAttribute("userRequests", userRequests);
         return "pageForStudents";
