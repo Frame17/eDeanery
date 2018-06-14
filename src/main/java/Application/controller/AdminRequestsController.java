487e392eb42e5f477d03f46d4f48a053aad2290f
kirill-kundik <goliathusua@gmail.com>
2018-06-14 12:52:48 +0300

    page or staff is now working

--- src/main/java/Application/controller/AdminRequestsController.java
+++ src/main/java/Application/controller/AdminRequestsController.java
@@ -1,15 +1,25 @@
 package Application.controller;
 
+import Application.model.UserRequest;
+import Application.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class AdminRequestsController {
 
+    @Autowired
+    private RequestService requestService;
+
     @GetMapping("/admin")
     public String getMainPage(Model model) {
-        return "pageForAdmins";
+        List<UserRequest> userRequests = requestService.findAll();
+        model.addAttribute("userRequests", userRequests);
+        return "pageForStaff";
     }
 
 }
