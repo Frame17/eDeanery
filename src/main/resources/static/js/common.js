92f099eea8ece7beb5b7b475e29a4162ed5899ac
Frame_17 <fedusov.serhiy@gmail.com>
2018-07-01 15:42:03 +0300

    action listener on "Reply" button

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,3 +1,7 @@
 function dropdownExtras() {
     document.getElementById("extrasDropdown").classList.toggle("show");
 }
+
+function openReplyForm() {
+    window.open("http://localhost:8080/replyForm","_self");
+}
