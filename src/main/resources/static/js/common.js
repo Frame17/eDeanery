b2af5b65c1759f80384b5f8a006ce011332578ee
Rickard31 <vitalka.marchenko@gmail.com>
2018-07-01 17:32:02 +0300

    Added UI for UserRequest creation

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -5,3 +5,11 @@ function dropdownExtras() {
 function openReplyForm() {
     window.open("http://localhost:8080/replyForm","_self");
 }
+
+function openAddRequestForm(){
+    window.open("http://localhost:8080/add", "_self");
+}
+
+function openUserPage(){
+    window.open("http://localhost:8080/userPage", "_self");
+}
