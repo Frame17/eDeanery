7ae1633d706ffcc15e2b884a1bd3187f54989014
Frame_17 <fedusov.serhiy@gmail.com>
2018-07-01 16:22:38 +0300

    added an id attribute foreach "Reply" button

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,6 +2,7 @@ function dropdownExtras() {
     document.getElementById("extrasDropdown").classList.toggle("show");
 }
 
+//the url should be changed
 function openReplyForm() {
     window.open("http://localhost:8080/replyForm","_self");
 }
