8207e5e41afcb4695fe35c1dc8365e6e7af4a8b6
Frame_17 <fedusov.serhiy@gmail.com>
2018-07-03 18:59:05 +0300

    added redirecting from reply page

--- src/main/java/Application/controller/AdminRequestsController.java
+++ src/main/java/Application/controller/AdminRequestsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -29,5 +30,10 @@ public class AdminRequestsController {
         UserRequest userRequest = requestService.findUserRequestById(id);
         model.addAttribute("userRequest", userRequest);
         return "replyForm";
+    }
+
+    @PostMapping("/replyForm")
+    public String getFromReplyForm() {
+        return "redirect:/admin";
     }
 }
