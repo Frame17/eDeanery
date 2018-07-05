f42d87d997103e5b469fba1790f4d96e8e7973fe
Frame_17 <fedusov.serhiy@gmail.com>
2018-07-05 14:35:10 +0300

    testing improvement

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,11 +1,22 @@
+import Application.App;
+import Application.controller.UserRequestsController;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
 public class MyTest {
+
+    @Autowired
+    private UserRequestsController userRequestsController;
 
     @Test
     public void myTest() {
-        assertEquals(1+1, 2);
+        assertThat(userRequestsController).isNotNull();
     }
 }
