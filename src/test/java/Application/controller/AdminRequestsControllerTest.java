c052cf5e2b901b211f688554d60231e05691904a
Frame_17 <fedusov.serhiy@gmail.com>
2018-07-06 14:10:02 +0300

    tests structure

---
+++ src/test/java/Application/controller/AdminRequestsControllerTest.java
@@ -0,0 +1,29 @@
+package Application.controller;
+
+import Application.service.RequestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AdminRequestsController.class)
+public class AdminRequestsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RequestService requestService;
+
+    @Test
+    public void getAdminPage() throws Exception {
+        mockMvc.perform(get("/admin")).andExpect(status().isOk());
+    }
+}
