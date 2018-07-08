c8beab42b2b515b1cca767bf0c7b190a3170f8c0
Frame_17 <fedusov.serhiy@gmail.com>
2018-07-09 00:27:07 +0300

    integration test finally work properly

--- src/test/java/Application/controller/AdminRequestsControllerIT.java
+++ src/test/java/Application/controller/AdminRequestsControllerIT.java
@@ -1,29 +1,23 @@
 package Application.controller;
 
-import Application.service.RequestService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(AdminRequestsController.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AdminRequestsControllerIT {
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private RequestService requestService;
+    private TestRestTemplate testRestTemplate;
 
     @Test
-    public void getAdminPage() throws Exception {
-        mockMvc.perform(get("/admin")).andExpect(status().isOk());
+    public void getAdminPage(){
+        ResponseEntity<String> stringResponseEntity = testRestTemplate.getForEntity("/admin", String.class);
+        System.out.println(stringResponseEntity);
     }
 }
