4040851b7544c5233ae9ee7d5394523c61681ac5
Frame_17 <fedusov.serhiy@gmail.com>
2018-07-10 16:22:00 +0300

    reviewed and refactored tests

--- src/test/java/Application/dao/UserRequestRepositoryTest.java
+++ src/test/java/Application/dao/UserRequestRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-//todo code refactoring, adding @Before and @BeforeAll methods
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -31,7 +31,7 @@ public class UserRequestRepositoryTest {
         for (int i = 0; i < 3; i++)
             userRequests.add(entityManager.persistFlushFind
                     (new UserRequest("testText" + i, "testTopic" + i)));
-        userRequests.forEach(System.out::println);
+
         assertThat(userRequests).isEqualTo(userRequestRepository.findAll());
     }
 
@@ -39,6 +39,7 @@ public class UserRequestRepositoryTest {
     public void findById() {
         UserRequest userRequest = entityManager.persistFlushFind
                 (new UserRequest("testText", " testTopic"));
+
         assertThat(userRequest).isEqualTo(userRequestRepository.findById(1));
     }
 
@@ -51,30 +52,32 @@ public class UserRequestRepositoryTest {
         UserRequest answeredUserRequest = new UserRequest("answered", "answered");
         answeredUserRequest.setResponseText("answer");
         entityManager.persistAndFlush(answeredUserRequest);
+
         assertThat(userRequestRepository.findAllByResponseTextNull()).isEqualTo(userRequests);
     }
 
     @Test
     public void updateRequestById() {
-        long id = (long) entityManager.persistAndGetId
-                (new UserRequest("testText", " testTopic"));
-        userRequestRepository.updateRequestById(id, "answer");
-        entityManager.refresh(entityManager.find(UserRequest.class, id));
-        assertThat(entityManager.find(UserRequest.class, id).getResponseText()).isNotNull();
+        UserRequest userRequest = new UserRequest("testText", " testTopic");
+        entityManager.persistAndFlush(userRequest);
+        userRequestRepository.updateRequestById(1, "answer");
+        entityManager.refresh(userRequest);
+
+        assertThat(entityManager.find(UserRequest.class, (long) 1).getResponseText()).isNotNull();
     }
 
     @Test
     public void deleteById() {
-        long id = (long) entityManager.persistAndGetId
-                (new UserRequest("testText", " testTopic"));
-        userRequestRepository.deleteById(id);
-        assertThat(entityManager.find(UserRequest.class, id)).isNull();
+        entityManager.persistAndFlush(new UserRequest("testText", " testTopic"));
+        userRequestRepository.deleteById(1);
+
+        assertThat(entityManager.find(UserRequest.class, (long) 1)).isNull();
     }
 
     @Test
     public void save() {
         userRequestRepository.save(new UserRequest("testText", "testTopic"));
-        System.out.println(entityManager.find(UserRequest.class, (long) 1));
+
         assertThat(entityManager.find(UserRequest.class, (long) 1)).isNotNull();
     }
 }
